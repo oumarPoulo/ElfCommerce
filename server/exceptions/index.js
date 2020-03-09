05a9ad998d12acaa8dbdb5951b100becbf1f362e
Alpha Diallo <aob.diallo@gmail.com>
2020-03-09 02:46:42 +0100

    add internal server error type

--- server/exceptions/index.js
+++ server/exceptions/index.js
@@ -30,14 +30,21 @@ function InvalidModelArgumentsError(msg) {
   this.statusCode = 400;
 }
 
+function InternalServerError(msg) {
+  Exception.call(this, msg);
+  this.statusCode = 500;
+}
+
 util.inherits(NoRecordFoundError, Exception);
 util.inherits(UnauthorisedError, Exception);
 util.inherits(BadRequestError, Exception);
 util.inherits(InvalidModelArgumentsError, Exception);
+util.inherits(InternalServerError, Exception);
 
 module.exports = {
   NoRecordFoundError,
   UnauthorisedError,
   BadRequestError,
   InvalidModelArgumentsError,
+  InternalServerError
 };
